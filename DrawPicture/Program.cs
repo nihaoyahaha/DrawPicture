d7fe9f4fcae702a56117a273f4416b2a7cb625e7
guyugui <2396394618@qq.com>
2025-06-09 18:00:17 +0800

    截图未完成

--- DrawPicture/Program.cs
+++ DrawPicture/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DrawKit;
@@ -8,16 +9,29 @@ using DrawKit;
 namespace DrawPicture
 {
     static class Program
-    {
-        /// <summary>
-        /// 应用程序的主入口点。
-        /// </summary>
-        [STAThread]
+	{
+		//[DllImport("user32.dll")]
+		//public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+		//[DllImport("user32.dll")]
+		//public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new  CanvasForm());
-        }
+			//Application.Run(new  DrawKit.Screenshot.CaptureForm());return;
+			var form = new ScreenshotApp();
+
+			// 注册 Alt+A 快捷键
+			//RegisterHotKey(form.Handle, ScreenshotApp.HOTKEY_ID, 1, (int)Keys.B);
+
+			Application.Run(form);
+
+			//UnregisterHotKey(form.Handle, ScreenshotApp.HOTKEY_ID);
+		}
     }
 }
